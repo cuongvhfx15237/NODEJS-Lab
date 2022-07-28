0b237a24c091e0ac1a36846d26aa8cb325af87ab
Vo Huy Cuong <cuongvh_fx15237@funix.edu.vn>
2022-07-29 02:52:33 +0700

    done Lab2.4

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
-app.use((req, res, next)=>{
-    console.log('in the middleware');
-    next(); //Allows the request to continue to te next middleware in line
+app.use('/', (req, res, next)=>{
+    console.log('This always runs!')
+    next();
 })
-app.use((req, res, next)=>{
+
+app.use('/add-product',(req, res, next)=>{
     console.log('in another middleware');
-    
+    res.send('<h1>the "add product" Page</h1>');
+
+})
+app.use('/',(req, res, next)=>{
+    console.log('in another middleware');
     res.send('<h1>Hello from Express</h1>');
 })
 
