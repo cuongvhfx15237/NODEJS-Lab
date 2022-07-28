6f3ef58acf9cf013928e4d7aa77b50a5e1aef2b4
Vo Huy Cuong <cuongvh_fx15237@funix.edu.vn>
2022-07-29 03:45:37 +0700

    use Express Router - done Lab 2.7

--- app.js
+++ app.js
@@ -3,26 +3,14 @@ const express = require('express');
 const bodyParser = require('body-parser')
 
 const app = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product',(req, res, next)=>{
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>>');
-})
-
-app.post('/product', (req, res, next)=>{
-    console.log(req.body)
-    res.redirect('/');
-})
-
-app.use('/',(req, res, next)=>{
-
-    res.send('<h1>Hello from Express</h1>');
-})
-
-
-const server = http.createServer(app);
 
 // http.createServer(rqListener);
-server.listen(3000);
+app.listen(3000);
