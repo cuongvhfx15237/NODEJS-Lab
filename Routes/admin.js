6189c3d2f9e68dd5c9c6aed682fcb3e521bdb4cd
Vo Huy Cuong <cuongvh_fx15237@funix.edu.vn>
2022-07-29 08:27:50 +0700

    page not found - done Lab_2.8

--- Routes/admin.js
+++ Routes/admin.js
@@ -3,7 +3,8 @@ const router = express.Router();
 router.get('/add-product',(req, res, next)=>{
 
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>>');
-})
+    
+});
 
 router.post('/product', (req, res, next)=>{
     console.log(req.body)
--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
-
+app.use((req, res, next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
+});
 // http.createServer(rqListener);
 app.listen(3000);
