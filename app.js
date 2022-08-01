514e5027aead3b544a53429247f66706f189b5ca
Vo Huy Cuong <cuongvh_fx15237@funix.edu.vn>
2022-08-01 19:17:04 +0700

    converting html to PUG - done Lab3.4

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
+    res.status(404).render('404')
 });
 // http.createServer(rqListener);
 app.listen(3000);
