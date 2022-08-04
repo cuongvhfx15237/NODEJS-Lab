dfa3cf675c1010baee1d5aa586147d28748190c9
Vo Huy Cuong <cuongvh_fx15237@funix.edu.vn>
2022-08-04 16:49:44 +0700

    Adding Controllers - Done Lab_4.1

--- Routes/admin.js
+++ Routes/admin.js
@@ -2,35 +2,15 @@ const path = require('path')
 
 const express=require('express');
 
-const rootDir = require('../util/path');
+const productsController = require('../controllers/products')
 
 const router = express.Router();
 
-const products = [];
 // /admin.add-product => get
-router.get('/add-product',(req, res, next)=>{
-
-    res.render('add-product', 
-    {
-        pageTitle: 'Add Products', 
-        path: '/admin/add-producrt',
-        formCSS: true, 
-        product: true,
-        activeProduct: true,
-        productCSS: true,
-        activeAddProduct: true
-    });
-    
-});
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product => post
-router.post('/add-product', (req, res, next)=>{
-    products.push({
-        title: req.body.title
-    })
-    res.redirect('/');
-})
+router.post('/add-product', productsController.postAddProduct);
 
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
