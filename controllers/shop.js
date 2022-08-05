bfb0b9d75852c90b57071ef407c7a08a6fe323c0
Vo Huy Cuong <cuongvh_fx15237@funix.edu.vn>
2022-08-05 20:47:47 +0700

    Rendering the Product detail view - done Lab4.10

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,10 +13,13 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const proId = req.params.productId;
   Product.findById(proId, product =>{
-    console.log(product);
+    res.render('shop/product-detail', {
+      product: product, 
+      pageTitle:product.title, 
+      path: '/products'
+    });
   });
-  res.redirect('/');
-}
+};
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
