373b8cd461e68fd54dd1049a936b8642a3087343
Vo Huy Cuong <cuongvh_fx15237@funix.edu.vn>
2022-08-05 08:06:09 +0700

    Adding the product ID to the Path - done Lab_4.7

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,16 +1,18 @@
-const path = require('path')
+const path = require('path');
 
-const express=require('express');
+const express = require('express');
 
-const productsController = require('../controllers/products')
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-// /admin.add-product => get
-router.get('/add-product', productsController.getAddProduct);
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-// /admin/add-product => post
-router.post('/add-product', productsController.postAddProduct);
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
