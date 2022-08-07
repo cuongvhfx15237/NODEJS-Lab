37c222d896567dec2cc5516f5065ad587f19c084
Vo Huy Cuong <cuongvh_fx15237@funix.edu.vn>
2022-08-07 14:11:37 +0700

    Fetching Products - Time to Practice - Done Lab_5.5

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,12 +2,19 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll()
+  .then(([rows, fieldData])=>{
     res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'All Products',
+      prods: rows,
+      pageTitle: 'all Products',
       path: '/products'
-    });
+    })
+  })
+  .catch(err => console.log(err));
+  res.render('shop/product-list', {
+    prods: products,
+    pageTitle: 'All Products',
+    path: '/products'
   });
 };
 
