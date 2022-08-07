163e8faed3c46aa84e6bbe7b7cf75b9a49c5187f
Vo Huy Cuong <vohuycuong.ks@gmail.com>
2022-08-07 21:36:45 +0700

    Syncing JS the Definitions to the Databases - Done Lab_5.10

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
-db.execute('SELECT * FROM products')
-.then(result =>{
-    console.log(result[0], result[1])
-})
-.catch(err => {
-    console.log(err)
-});
+// sequelize.execute('SELECT * FROM products')
+// .then(result =>{
+//     console.log(result[0], result[1])
+// })
+// .catch(err => {
+//     console.log(err)
+// });
 
 const app = express();
 
@@ -30,4 +30,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize.sync()
+.then(result => {
+    // console.log(result);
+    app.listen(3000);
+})
+.catch(err => {
+    console.log(err);
+})
