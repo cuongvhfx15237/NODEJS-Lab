a8e0ab23ddb38b6cb202dc895a7bb344cc70bb5c
Vo Huy Cuong <cuongvh_fx15237@funix.edu.vn>
2022-08-09 21:50:04 +0700

    Fixing the 'add products' functionary - Done Lab 6.13

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,9 +14,9 @@ exports.getAddProduct = (req, res, next) => {
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
   const price = req.body.price;
-  const imageUrl = req.body.imageUrl;
   const description = req.body.description;
-  const product = new Product(title, price, imageUrl, description);
+  const imageUrl = req.body.imageUrl;
+  const product = new Product(title, price, description, imageUrl);
   product.save()
     .then(result => {
       // console.log(result);
