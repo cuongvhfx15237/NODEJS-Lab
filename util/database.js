58e8899a52c2220f623594bc4db252d555a20549
Vo Huy Cuong <cuongvh_fx15237@funix.edu.vn>
2022-08-09 18:41:12 +0700

    Installing Mongodb Driver - Done Lab 6.1

--- util/database.js
+++ util/database.js
@@ -1,8 +1,17 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'admin', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = callback => {
 
-module.exports = sequelize;
+  MongoClient.connect('mongodb+srv://cuongvhfx15237:adminitration@cuongvhfx15237.k0pn0dz.mongodb.net/?retryWrites=true&w=majority')
+  .then(client => {
+    console.log('Connected: !!!!');
+    callback(client)
+  })
+  .catch(err => {
+    console.log(err);
+  
+  })
+}
+
+module.exports = mongoConnect;
