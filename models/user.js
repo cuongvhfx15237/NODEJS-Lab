eb5e7f3344ee0848d9bb32ba2ac0631c2f23fc37
Vo Huy Cuong <vohuycuong.ks@gmail.com>
2022-08-09 23:38:01 +0700

    Storing the User in Database - Done Lab 6.15

--- models/user.js
+++ models/user.js
@@ -1,14 +1,25 @@
-const Sequelize = require('sequelize')
-const sequelize = require('../util/database')
-const User = sequelize.define('user', {
-    id: {
-        type:Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    name: Sequelize.STRING,
-    email: Sequelize.STRING
-})
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
+
+class User {
+    construc(username, email) {
+        this.name = username;
+        this.email = email;
+    }
+    save() {
+        const db = getDb();
+        return db.collection('users').insertIOne(this);
+    }
+    static findById(userId) {
+        const db = getDb();
+        return db.collection('users')
+        .findOne({_id: new mongodb.ObjectId(userId) })
+        .then(user => {
+            console.log(user);
+            return user;
+        })
+        .catch(err => { console.log(err)})
+    }
+}
 
 module.exports = User;
