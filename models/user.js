521858f7c5632e51a144721fc6bad6810fc1d0dd
Vo Huy Cuong <cuongvh_fx15237@funix.edu.vn>
2022-08-10 19:09:36 +0700

    Working on Cart Item and Orders - Done Lab 6.16

--- models/user.js
+++ models/user.js
@@ -2,14 +2,30 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class User {
-    construc(username, email) {
+    construc(username, email, cart, id) {
         this.name = username;
         this.email = email;
+        this.cart = cart; //{item: []} <- create item in user
+        this._id = id;
     }
     save() {
         const db = getDb();
         return db.collection('users').insertIOne(this);
     }
+
+    addToCart(product) {
+        // const cartProduct = this.cart.items.findIndex(cp => {
+        //     return cp.id === product._id
+        // })
+        const updatedCart = { item: [{...product, quantity: 1}]};
+        const db = getDb();
+        return db.collection('users')
+        .updateOne(
+            {_id: new ObjectId(this._id)}, 
+            {$set: {cart: updatedCart}}
+            );
+    }
+
     static findById(userId) {
         const db = getDb();
         return db.collection('users')
