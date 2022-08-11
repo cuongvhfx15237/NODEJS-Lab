053d6f77756658fc47e2130dbbcd1de55ad25298
Vo Huy Cuong <cuongvhfx15237@funix.edu.vn>
2022-08-11 16:23:46 +0700

    Getting Order - End Lab 6

--- models/user.js
+++ models/user.js
@@ -99,6 +99,10 @@ class User {
     
     getOrders() {
       const db = getDb();
+      return db
+      .collection('orders')
+      .find({'user._id': new ObjectId(this._id)})
+      .toArray();
     }
 
     static findById(userId) {
