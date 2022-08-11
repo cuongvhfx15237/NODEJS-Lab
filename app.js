6885226512da107e98e4ac0ad7c79f3b4d579bf1
Vo Huy Cuong <cuongvh_fx15237@funix.edu.vn>
2022-08-11 19:08:44 +0700

    Connected server with mogoose - Done Lab 7.1

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -32,7 +32,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(()=> {
-
+mongoose
+.connect('mongodb+srv://cuongvhfx15237:adminitration@cuongvhfx15237.k0pn0dz.mongodb.net/?retryWrites=true&w=majority')
+.then( result => {
+  console.log('Connect: !!!!')
   app.listen(3000);
+}).catch(err => {
+  console.log(err);
 })
