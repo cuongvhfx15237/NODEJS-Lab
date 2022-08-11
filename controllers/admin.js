29745566146f1bfea5b084adb1f021e634510fdb
Vo Huy Cuong <cuongvh_fx15237@funix.edu.vn>
2022-08-11 20:19:42 +0700

    Updating Products - Done Lab 7.6

--- controllers/admin.js
+++ controllers/admin.js
@@ -37,7 +37,6 @@ exports.getEditProduct = (req, res, next) => {
   }
   const prodId = req.params.productId;
     Product.findById(prodId)
-    // Product.findByPk(prodId)
     .then(product => {
       if (!product) {
         return res.redirect('/');
@@ -58,15 +57,14 @@ exports.postEditProduct = (req, res, next) => {
   const updatedDesc = req.body.description;
   const updatedImageUrl = req.body.imageUrl;
   const prodId = req.body.productId;
-  
-      const product = new Product(
-        updatedTitle, 
-        updatedPrice, 
-        updatedDesc,
-        updatedImageUrl, 
-        prodId
-        );
+  Product.findById(prodId)
+  .then(product => {
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.description = updatedDesc;
+    product.imageUrl = updatedImageUrl;
     product.save()
+  })
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
@@ -75,7 +73,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
- Product.fetchAll()
+ Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
