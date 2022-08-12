81ebb994b3c13faa3ef760a3db1bdbed6e5c4a17
Vo Huy Cuong <cuongvh_fx15237@funix.edu.vn>
2022-08-12 07:56:46 +0700

    Adding and using a user Model - Done Lab_7.8

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
 
 const app = express();
 
@@ -14,18 +14,19 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById("62f289973bc5669fd0072751")
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id) ;
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("62f5a4fde3e920d2f578354a")
+    .then(user => {
+      req.user = user ;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -35,8 +36,22 @@ app.use(errorController.get404);
 mongoose
 .connect('mongodb+srv://cuongvhfx15237:adminitration@cuongvhfx15237.k0pn0dz.mongodb.net/shop?retryWrites=true&w=majority')
 .then( result => {
+  User.findOne()
+  .then(user =>{
+    if (!user) {
+      const user = new User({
+        name: 'Cuong',
+        email: 'cuongvhfx@funix.edu.vn',
+        cart: {
+          items: []
+        }
+      })
+      user.save();
+    }
+  })
   console.log('Connect: !!!!')
   app.listen(3000);
-}).catch(err => {
+})
+.catch(err => {
   console.log(err);
 })
