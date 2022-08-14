145b87f5d997df5a7359fe726ac61768b1787d70
Vo Huy Cuong <cuongvh_fx15237@funix.edu.vn>
2022-08-14 08:31:15 +0700

    Creating and Getting Order - Donw Lab7.14

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,7 @@
 const Product = require('../models/product');
 
+const Order = require('../models/order');
+
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then(products => {
@@ -82,9 +84,23 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
-  req.user
-    .addOrder()
+  req.user.populate('cart.items.productId')
+  .then(user => {
+    const products = user.cart.items.map(i => {
+      return {
+        quantity: i.quantity, product: i.productId
+      }
+    })
+    const order = new Order({
+      user: {
+        name: req.user.name,
+        userId: req.user
+      },
+      products: products
+    })
+    console.log(order)
+   return order.save();
+  })
     .then(result => {
       res.redirect('/orders');
     })
