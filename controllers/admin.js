badcb9d1f39dcd1d536d3c9fd392863e88a1f9bc
Vo Huy Cuong <cuongvh_fx15237@funix.edu.vn>
2022-10-30 00:50:42 +0700

    done all lab 9

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,8 @@
 const Product = require('../models/product');
+const {validationResult} = require('express-validator')
 
 exports.getAddProduct = (req, res, next) => {
+  // const errors = validationResult(req);
   if (!req.session.isLoggedIn) {
     return res.redirect('/login')
   }
@@ -8,20 +10,41 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     editing: false,
+    hasError: false,
+    errorMessage: null,
+    validationErrors:[],
   });
+
 };
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const imageUrl = req.body.imageUrl;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+   return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/edit-product',
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        imageUrl: imageUrl,
+        price: price,
+        description: description,
+      },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array(),
+    });
+  }
   const product = new Product(
     {title: title,
     price: price,
     description: description,
     imageUrl: imageUrl,
-    userId: req.user._id
+    userId: req.user
   })
   product.save()
     .then(result => {
@@ -50,37 +73,66 @@ exports.getEditProduct = (req, res, next) => {
         path: '/admin/edit-product',
         editing: editMode,
         product: product,
+        hasError: false,
+        errorMessage: null,
+        validationErrors: [],
       });
     })
     .catch(err => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
   const updatedTitle = req.body.title;
   const updatedPrice = req.body.price;
-  const updatedDesc = req.body.description;
   const updatedImageUrl = req.body.imageUrl;
-  const prodId = req.body.productId;
+  const updatedDesc = req.body.description;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+   return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: true,
+      hasError: true,
+      product: {
+        title: updatedTitle,
+        imageUrl: updatedImageUrl,
+        price: updatedPrice,
+        description: updatedDesc,
+        _id: prodId,
+      },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array(),
+    });
+  }
+
   Product.findById(prodId)
   .then(product => {
+    //console log this here to check what is this
+    if (product.userId.toString() !== req.user._id.toString()){
+      return res.redirect('/');
+    }
     product.title = updatedTitle;
     product.price = updatedPrice;
     product.description = updatedDesc;
     product.imageUrl = updatedImageUrl;
-    product.save()
-  })
+    return product.save()
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
+  })
     })
     .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
- Product.find()
+  // chú ý tại vị trí này, cần đễ ý tại phần userId bên trong product
+  // nó chĩ render ra sản phẩm có chứa userId trùng với id đang login
+ Product.find({userId: req.user._id})
 //  .select('title price -_id')
 //     .populate('userId', 'name')
     .then(products => {
+      console.log(products)
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
@@ -92,7 +144,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByIdAndRemove(prodId)
+  Product.deleteOne({_id: prodId, userId: req.user._id})
     .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
